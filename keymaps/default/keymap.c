bca46a0d280fc4cb5c0c09805402bc94c80bd305
katsuemon <katsuemon@gmail.com>
2024-05-16 09:26:19 +0900

    oled_status

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -22,7 +22,7 @@ enum led_flag {
     _NUML = 0x80
 };
 
-int led_state[4] = { 0, 0 };
+int led_layer_state[4] = { 0, 0, 0, 0 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* PC Layout
@@ -61,19 +61,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASEPC] = LAYOUT_compy_jis(
         KC_ESC,  KC_APP,     KC_F1,     KC_F2,    KC_F3,    KC_F4,    KC_F5,      KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,      KC_MUTE, KC_VOLD, KC_VOLU,
-        KC_GRV,    JP_1,   JP_2,   JP_3,   JP_4,   JP_5,   JP_6,   JP_7,   JP_8,   JP_9,   JP_0,JP_MINS,JP_CIRC, JP_YEN,KC_BSPC,      KC_HOME, _______, KC_END,
+        KC_GRV,    JP_1,   JP_2,   JP_3,   JP_4,   JP_5,   JP_6,   JP_7,   JP_8,   JP_9,   JP_0,JP_MINS,JP_CIRC, JP_YEN,KC_BSPC,      KC_HOME, TG(_NMPDPC), KC_END,
         KC_TAB,       JP_Q,   JP_W,   JP_E,   JP_R,   JP_T,   JP_Y,   JP_U,   JP_I,   JP_O,   JP_P,   JP_AT,   JP_LBRC,KC_ENTER,      KC_DEL,  KC_CLR,  KC_PMNS,
         KC_CAPS,       JP_A,   JP_S,   JP_D,   JP_F,   JP_G,   JP_H,   JP_J,   JP_K,   JP_L,JP_SCLN,JP_COLN,JP_RBRC,                  _______, KC_INS,  KC_PPLS,
         KC_LSFT,        JP_Z,    JP_X,    JP_C,    JP_V,    JP_B,    JP_N,    JP_M, JP_COMM,  JP_DOT, JP_SLSH, JP_BSLS, KC_RSFT,      KC_PGUP, KC_UP,   KC_PGDN,
         KC_LCTL, KC_LGUI, KC_LALT, JP_MHEN,             KC_SPC,            JP_HENK, JP_KANA, KC_RALT, KC_RGUI, _______, KC_RCTL,      KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_NMPDPC] = LAYOUT_compy_jis(
-        _______,_______,   _______,   _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,      _______, _______, _______,
-        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,      _______, _______, _______,
-        _______,   _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,     _______,      _______, _______, _______,
-        _______,    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,                  _______, _______, _______,
-        _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______,
-        _______, _______, _______, _______,            _______,            _______, _______, _______, _______, _______, _______,      _______, _______, _______
+        _______,_______,   _______,   _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,      KC_PSLS, KC_PAST, KC_PMNS,
+        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,      _______, _______, KC_PPLS,
+        _______,   _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,     _______,      KC_P7,   KC_P8,   KC_P9,
+        _______,    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,                  KC_P4,   KC_P5,   KC_P6,
+        _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      KC_P1,   KC_P2,   KC_P3,
+        _______, _______, _______, _______,            _______,            _______, _______, _______, _______, _______, _______,      KC_P0,   KC_PDOT, KC_PENT
     ),
     [_BASE68] = LAYOUT_compy_jis(
         _______,_______,   _______,   _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,      _______, _______, _______,
@@ -101,10 +101,43 @@ static void render_logo(void) {
     static const char PROGMEM compyx_logo[] = {
         0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
         0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
-        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x00
+        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0x20, 0x00
     };
 
     oled_write_P(compyx_logo, false);
+}
+
+
+static void render_layer(void) {
+    static const char PROGMEM g_layer[] = {
+        0x01, 0x02, 0x03, 0x04, 0x00
+    };
+
+    oled_write_P(g_layer, false);
+}
+
+static void render_pc(void) {
+    static const char PROGMEM g_pc[] = {
+        0x05, 0x06, 0x07, 0x00
+    };
+
+    oled_write_P(g_pc, false);
+}
+
+static void render_x68k(void) {
+    static const char PROGMEM g_x68k[] = {
+        0x0B, 0x0C, 0x0D, 0x00
+    };
+
+    oled_write_P(g_x68k, false);
+}
+
+static void render_num(void) {
+    static const char PROGMEM g_num[] = {
+        0x08, 0x09, 0x0A, 0x00
+    };
+
+    oled_write_P(g_num, false);
 }
 
 static void render_kana_on(void) {
@@ -162,7 +195,7 @@ static void render_zenkaku_on(void) {
 
     oled_write_P(zenkaku_on, false);
 }
-/*
+
 static void render_kana_off(void) {
     static const char PROGMEM kana_off[] = {
         0xB0, 0xB1, 0xB2, 0x00
@@ -204,7 +237,7 @@ static void render_ins_off(void) {
 }
 
 static void render_hira_off(void) {
-    stSatic const char PROGMEM hira_off[] = {
+    static const char PROGMEM hira_off[] = {
         0xCD, 0xCE, 0xCF, 0x00
     };
 
@@ -218,25 +251,72 @@ static void render_zenkaku_off(void) {
 
     oled_write_P(zenkaku_off, false);
 }
-*/
+
 bool oled_task_user(void) {
+    const led_t led_state = host_keyboard_led_state();
+    if (led_state.caps_lock) { led_layer_state[get_highest_layer(layer_state)] |= _CAPS; } else { led_layer_state[get_highest_layer(layer_state)] &= ~_CAPS;};
+    oled_set_cursor(0,0);
+
     render_logo();
-    render_kana_on();
-    render_roma_on();
-    render_code_on();
-    render_caps_on();
-    render_ins_on();
-    render_hira_on(); 
-    render_zenkaku_on();
-    /*
-    render_kana_off();
-    render_roma_off();
-    render_code_off();
-    render_caps_off();
-    render_ins_off();
-    render_hira_off();
-    render_zenkaku_off();
-    */
+
+    // Layer indicator
+    oled_set_cursor(14,2);
+    render_layer();
+    switch(get_highest_layer(layer_state)) {
+        case _BASEPC:
+            render_pc();
+            break;
+        case _NMPDPC:
+            render_num();
+            break;
+        case _BASE68:
+            render_x68k();
+            break;
+        case _NMPD68:
+            render_num();
+            break;
+        default:
+            oled_write_P(PSTR("UNDEF\n"),false);
+    }
+
+    // LED State indicator
+    oled_set_cursor(0,3);
+    if (led_layer_state[get_highest_layer(layer_state)] & _KANA) {
+        render_kana_on();
+    } else {
+        render_kana_off();
+    }
+    if (led_layer_state[get_highest_layer(layer_state)] & _ROMA) {
+        render_roma_on();
+    } else {
+        render_roma_off();
+    }
+    if (led_layer_state[get_highest_layer(layer_state)] & _CODE) {
+        render_code_on();
+    } else {
+        render_code_off();
+    }
+    if (led_layer_state[get_highest_layer(layer_state)] & _CAPS) {
+        render_caps_on();
+    } else {
+        render_caps_off();
+    }
+    if (led_layer_state[get_highest_layer(layer_state)] & _INS) {
+        render_ins_on();
+    } else {
+        render_ins_off();
+    }
+    if (led_layer_state[get_highest_layer(layer_state)] & _HIRA) {
+        render_hira_on();
+    } else {
+        render_hira_off();
+    }
+    if (led_layer_state[get_highest_layer(layer_state)] & _ZENK) {
+        render_zenkaku_on();
+    } else {
+        render_zenkaku_off();
+    }
+
     return false;
 }
 
