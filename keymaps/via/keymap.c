492d112b022c07208bd645d215a5e550054685de
katsuemon <katsuemon@gmail.com>
2024-06-11 17:24:17 +0900

    X68000Z 対応

--- keymaps/via/keymap.c
+++ keymaps/via/keymap.c
@@ -119,12 +119,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MO_68  , _______, _______, _______,            _______,            _______, _______, _______, _______, _______, MO_68,        KC_P0,   KC_PDOT, KC_PENT
     ),
     [_BASE68] = LAYOUT_compy_jis(
-        KC_BRK, _______,     KC_F1,     KC_F2,    KC_F3,    KC_F4,    KC_F5,      KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,      KC_MUTE, KC_VOLD, _______,
+        JP_ZKHK,_______,     KC_F1,     KC_F2,    KC_F3,    KC_F4,    KC_F5,      KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,      KC_MUTE, KC_VOLD, KC_VOLU,
         KC_ESC,    JP_1,   JP_2,   JP_3,   JP_4,   JP_5,   JP_6,   JP_7,   JP_8,   JP_9,   JP_0,JP_MINS,JP_CIRC, JP_YEN,KC_BSPC,      KC_RGUI, KC_LGUI, KC_PAUS,
         KC_TAB,       JP_Q,   JP_W,   JP_E,   JP_R,   JP_T,   JP_Y,   JP_U,   JP_I,   JP_O,   JP_P,   JP_AT,   JP_LBRC,KC_ENTER,      KC_HOME, KC_NUM,  KC_DEL,
         KC_LCTL,       JP_A,   JP_S,   JP_D,   JP_F,   JP_G,   JP_H,   JP_J,   JP_K,   JP_L,JP_SCLN,JP_COLN,JP_RBRC,                  TG_NM68, KC_INS,  KC_END,
-        KC_LSFT,        JP_Z,    JP_X,    JP_C,    JP_V,    JP_B,    JP_N,    JP_M, JP_COMM,  JP_DOT, JP_SLSH, JP_BSLS, KC_RSFT,      KC_PGDN, KC_UP,   KC_PGUP,
-        KC_F11,  KC_CAPS, KC_LALT, KC_INT5,             KC_SPC,            KC_INT4, KC_INT2, KC_RALT, KC_LSCR, KC_PSCR, KC_F12,       KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LSFT,        JP_Z,    JP_X,    JP_C,    JP_V,    JP_B,    JP_N,    JP_M, JP_COMM,  JP_DOT, JP_SLSH, JP_BSLS, KC_RSFT,      KC_PGUP, KC_UP,   KC_PGDN,
+        KC_F11,  KC_CAPS, KC_LALT, KC_INT5,             KC_SPC,            KC_INT4, KC_INT2, KC_RALT, KC_SCRL, KC_PSCR, KC_F12,       KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_NMPD68] = LAYOUT_compy_jis(
         _______,_______,   _______,   _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,      _______, _______, _______,
